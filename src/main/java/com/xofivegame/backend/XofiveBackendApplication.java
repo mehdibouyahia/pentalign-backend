2548b41fe94c9d352656473bd14515be33a3d9db
mehdibouyahia <elmehdibouyahia@gmail.com>
2025-04-14 23:48:51 +0200

    Initial Spring Boot server setup

---
+++ src/main/java/com/xofivegame/backend/XofiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xofivegame.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XofiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XofiveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xofivegame/backend/XofiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xofivegame.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XofiveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
